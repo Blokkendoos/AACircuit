77e4c7e8ba389603d89e82e866a4d6a72c38a291
blokkendoos <jvo@chaosgeordend.nl>
2020-03-11 12:17:26 +0100

    Custom cursor test

--- application/main_window.py
+++ application/main_window.py
@@ -5,6 +5,7 @@ AACircuit
 
 import os
 import sys
+import cairo
 
 from application.component_library import ComponentLibrary
 from application.grid import Grid
@@ -13,7 +14,8 @@ from application.grid_canvas import GridCanvas
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk  # noqa: E402
-from gi.repository import GLib  # noqa: E402
+# from gi.repository import GLib  # noqa: E402
+from gi.repository import GdkPixbuf  # noqa: E402
 
 
 class MainWindow(Gtk.Window):
@@ -61,6 +63,8 @@ class MainWindow(Gtk.Window):
 
         self.btn_cur[0].set_active(True)
 
+        self._init_cursors()
+
         # connect signals
 
         builder.connect_signals(self)
@@ -89,25 +93,18 @@ class MainWindow(Gtk.Window):
 
         fixed.add(grid_canvas)
 
+    def _init_cursors(self):
+        self._cursor = []
+        for i in range(1, 4):
+            self._cursor.append(GdkPixbuf.Pixbuf.new_from_file("buttons/c{0}.png".format(i)))
+
     def _on_toggled_cursor(self, button, data=None):
 
         if button.get_active():
 
-            # https://askubuntu.com/questions/138336/how-to-change-the-cursor-to-hourglass-in-a-python-gtk3-application
-            # cursor = Gdk.Cursor.new(Gdk.CursorType.WATCH)
-
             name = button.get_name()
-
-            # https://developer.gnome.org/gdk3/stable/gdk3-Cursors.html#gdk-cursor-new-from-name
-            if name == "btn_cur1":
-                cursor = Gdk.Cursor.new(Gdk.CursorType.LEFT_PTR)
-            elif name == "btn_cur2":
-                cursor = Gdk.Cursor.new(Gdk.CursorType.CROSSHAIR)
-            elif name == "btn_cur3":
-                cursor = Gdk.Cursor.new(Gdk.CursorType.WATCH)
-            elif name == "btn_cur4":
-                cursor = Gdk.Cursor.new(Gdk.CursorType.CROSSHAIR)
-            self.get_root_window().set_cursor(cursor)
+            btn = int(name[-1])
+            self.custom_cursor(btn)
 
             # disable the other cursor buttons
             for btn in self.btn_cur:
@@ -121,3 +118,47 @@ class MainWindow(Gtk.Window):
     def _on_close_clicked(self, button):
         print("Closing application")
         Gtk.main_quit()
+
+    def custom_cursor(self, btn):
+        # https://askubuntu.com/questions/138336/how-to-change-the-cursor-to-hourglass-in-a-python-gtk3-application
+        # cursor = Gdk.Cursor.new(Gdk.CursorType.WATCH)
+
+        # https://developer.gnome.org/gdk3/stable/gdk3-Cursors.html#gdk-cursor-new-from-name
+        if btn == 1:
+            cursor = Gdk.Cursor.new(Gdk.CursorType.LEFT_PTR)
+        elif btn == 2:
+            cursor = Gdk.Cursor.new(Gdk.CursorType.CROSSHAIR)
+        elif btn == 3:
+            cursor = Gdk.Cursor.new(Gdk.CursorType.WATCH)
+        elif btn == 4:
+            cursor = Gdk.Cursor.new(Gdk.CursorType.CROSSHAIR)
+        self.get_root_window().set_cursor(cursor)
+
+    def custom_cursor_XXX(self, btn):
+        # display = self.get_screen().get_display()
+        # pb = GdkPixbuf.Pixbuf.new(GdkPixbuf.Colorspace.RGB, False, 8, 100, 100)
+        # cursor = Gdk.Cursor.new_from_pixbuf(display, pb, 100, 100)
+        # pb = GdkPixbuf.Pixbuf.new(GdkPixbuf.Colorspace.RGB, False, 8, 100, 100)
+        # self.get_root_window().set_cursor(cursor)
+
+        # https: // stackoverflow.com / questions / 55283386 / noisy - cairo - created - custom - cursor - in -gtk3
+        display = self.get_screen().get_display()
+        # pb = GdkPixbuf.Pixbuf.new(GdkPixbuf.Colorspace.RGB, True, 8, 2 * radius, 2 * radius)
+        pb = GdkPixbuf.Pixbuf.new_from_file("buttons/c{0}.bmp".format(btn))
+        surface = Gdk.cairo_surface_create_from_pixbuf(pb, 0, None)
+
+        # context = cairo.Context(surface)
+        cursor = Gdk.Cursor.new_from_pixbuf(display, pb, 16, 16)
+        self._grid_canvas.drawing_area.get_screen().get_root_window().set_cursor(cursor)
+
+    def custom_cursor_YYY(self, radius):
+        display = self.get_screen().get_display()
+        pb = GdkPixbuf.Pixbuf.new(GdkPixbuf.Colorspace.RGB, True, 8, 2 * radius, 2 * radius)
+        surface = Gdk.cairo_surface_create_from_pixbuf(pb, 0, None)
+        context = cairo.Context(surface)
+        context.arc(radius, radius, radius, 0, 2 * np.pi)
+        context.set_source_rgba(0, 0, 0, 1)
+        context.stroke()
+        pbdrawn = Gdk.pixbuf_get_from_surface(surface, 0, 0, surface.get_width(), surface.get_height())
+        cursor = Gdk.Cursor.new_from_pixbuf(display, pbdrawn, radius, radius)
+        self.darea.get_screen().get_root_window().set_cursor(cursor)
