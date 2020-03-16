3c317387da8dbbdf4fccf090d8d6de65e91a33c1
blokkendoos <jvo@chaosgeordend.nl>
2020-03-16 11:48:00 +0100

    Moved literals to application

---
+++ application/__init__.py
@@ -0,0 +1,18 @@
+"""
+AACircuit
+2020-03-02 JvO
+"""
+
+# grid
+FONTSIZE = 12
+GRIDSIZE_W = 7
+GRIDSIZE_H = 16
+
+# row and column selection
+REMOVE = 'remove'
+INSERT = 'insert'
+IDLE = 0
+SELECTING = 1
+SELECTED = 2
+COL = 'col'
+ROW = 'row'
