88a1f562b9d53b4e88b496f4026e525dddae7378
blokkendoos <jvo@chaosgeordend.nl>
2020-03-23 13:31:00 +0100

    Separated Pos class defintion

--- application/grid_view.py
+++ application/grid_view.py
@@ -8,63 +8,16 @@ from pubsub import pub
 from numpy import sign
 
 from application import GRIDSIZE_W, GRIDSIZE_H
-from application import INSERT, REMOVE, HORIZONTAL, VERTICAL
+from application import INSERT, HORIZONTAL, VERTICAL
 from application import IDLE, SELECTING, SELECTED
 from application import COMPONENT, COL, ROW, RECT, LINE, MAG_LINE
 from application import LINE_HOR, LINE_VERT, TERMINAL1, TERMINAL2, TERMINAL3, TERMINAL4
 from application.symbol_view import SymbolView
+from application import Pos
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk  # noqa: E402
-
-
-class Pos(object):
-    """A position on the grid (canvas)."""
-
-    def __init__(self, x, y):
-        self._x = int(x)
-        self._y = int(y)
-
-    def __add__(self, other):
-        x = self._x + other.x
-        y = self._y + other.y
-        return Pos(x, y)
-
-    def __sub__(self, other):
-        x = self._x - other.x
-        y = self._y - other.y
-        return Pos(x, y)
-
-    def __str__(self):
-        return "x:{0} y:{1}".format(self._x, self._y)
-
-    @property
-    def x(self):
-        return self._x
-
-    @property
-    def y(self):
-        return self._y
-
-    @property
-    def xy(self):
-        return (self._x, self._y)
-
-    def snap_to_grid(self):
-        """Set position to the nearest (canvas) grid coordinate."""
-        (x, y) = (self._x, self._y)
-        x -= x % GRIDSIZE_W
-        y -= y % GRIDSIZE_H
-        self._x = int(x)
-        self._y = int(y)
-
-    def grid_rc(self):
-        """Map canvas (x,y) position to grid (col,row) coordinates."""
-        (x, y) = (self._x, self._y)
-        x /= GRIDSIZE_W
-        y /= GRIDSIZE_H
-        return Pos(x, y)
 
 
 class GridView(Gtk.Frame):
---
+++ application/pos.py
@@ -0,0 +1,54 @@
+"""
+AACircuit
+2020-03-02 JvO
+"""
+
+from application import GRIDSIZE_W, GRIDSIZE_H
+
+
+class Pos(object):
+    """A position on the grid (canvas)."""
+
+    def __init__(self, x, y):
+        self._x = int(x)
+        self._y = int(y)
+
+    def __add__(self, other):
+        x = self._x + other.x
+        y = self._y + other.y
+        return Pos(x, y)
+
+    def __sub__(self, other):
+        x = self._x - other.x
+        y = self._y - other.y
+        return Pos(x, y)
+
+    def __str__(self):
+        return "x:{0} y:{1}".format(self._x, self._y)
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def xy(self):
+        return (self._x, self._y)
+
+    def snap_to_grid(self):
+        """Set position to the nearest (canvas) grid coordinate."""
+        (x, y) = (self._x, self._y)
+        x -= x % GRIDSIZE_W
+        y -= y % GRIDSIZE_H
+        self._x = int(x)
+        self._y = int(y)
+
+    def grid_rc(self):
+        """Map canvas (x,y) position to grid (col,row) coordinates."""
+        (x, y) = (self._x, self._y)
+        x /= GRIDSIZE_W
+        y /= GRIDSIZE_H
+        return Pos(x, y)
