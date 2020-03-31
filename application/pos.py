f3ebcdd233347f17d7b899e752673be750ff8c1e
blokkendoos <jvo@chaosgeordend.nl>
2020-03-31 19:52:48 +0200

    Restore methods that were inadvertently removed

--- application/pos.py
+++ application/pos.py
@@ -62,9 +62,17 @@ class Pos(object):
     def x(self):
         return self._x
 
+    @ x.setter
+    def x(self, value):
+        self._x = value
+
     @property
     def y(self):
         return self._y
+
+    @ y.setter
+    def y(self, value):
+        self._y = value
 
     @property
     def xy(self):
