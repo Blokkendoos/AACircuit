4640815f9076168511f2d8907c626381a6cf5f69
blokkendoos <jvo@chaosgeordend.nl>
2020-03-31 08:09:47 +0200

    Refactor: separate MenuBar class

---
+++ application/menubar.py
@@ -0,0 +1,79 @@
+"""
+AACircuit
+2020-03-02 JvO
+"""
+
+from pubsub import pub
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk  # noqa: E402
+
+
+class MenuBar(object):
+
+    def __init__(self, builder, grid_view):
+
+        self.grid_view = grid_view
+
+        # file menu
+        menu_new = builder.get_object('new_file')
+        menu_open = builder.get_object('open_file')
+        self.menu_save = builder.get_object('save_file')
+        self.menu_save_as = builder.get_object('save_as_file')
+
+        menu_new.connect('activate', self.on_menu_file)
+        menu_open.connect('activate', self.on_menu_file)
+        self.menu_save.connect('activate', self.on_menu_file)
+        self.menu_save_as.connect('activate', self.on_menu_file)
+
+        self.menu_save.set_sensitive(False)
+
+        # edit menu
+        self.menu_copy = builder.get_object('copy')
+        self.menu_cut = builder.get_object('cut')
+        self.menu_paste = builder.get_object('paste')
+        self.menu_delete = builder.get_object('delete')
+        self.menu_undo = builder.get_object('undo')
+
+        self.menu_copy.connect('activate', self.on_menu_edit)
+        self.menu_cut.connect('activate', self.on_menu_edit)
+        self.menu_paste.connect('activate', self.on_menu_edit)
+        self.menu_delete.connect('activate', self.on_menu_edit)
+        self.menu_undo.connect('activate', self.on_undo)
+
+        self.menu_copy.set_sensitive(False)
+        self.menu_cut.set_sensitive(False)
+        self.menu_paste.set_sensitive(False)
+        self.menu_delete.set_sensitive(False)
+        self.menu_undo.set_sensitive(False)
+
+        pub.subscribe(self.on_file_opened, 'FILE_OPENED')
+        pub.subscribe(self.on_selection_changed, 'SELECTION_CHANGED')
+        pub.subscribe(self.on_undo_changed, 'UNDO_CHANGED')
+
+    def on_menu_file(self, item):
+        name = Gtk.Buildable.get_name(item).upper()
+        pub.sendMessage(name)
+
+    def on_file_opened(self):
+        # only when a file is opened, enable 'save' in the File menu
+        self.menu_save.set_sensitive(True)
+
+    def on_menu_edit(self, item):
+        # menu cut|copy|paste
+        name = Gtk.Buildable.get_name(item)
+        pos, width, height = self.grid_view.drag_rect
+        pub.sendMessage(name.upper(), pos=pos, rect=(width, height))
+
+    def on_selection_changed(self, selected=False):
+        # enable the cut and copy menu only when one or more objects are selected
+        self.menu_copy.set_sensitive(selected)
+        self.menu_cut.set_sensitive(selected)
+
+    def on_undo_changed(self, undo=False):
+        # enable undo only if the undo-stack is not empty
+        self.menu_undo.set_sensitive(undo)
+
+    def on_undo(self, button):
+        pub.sendMessage('UNDO')
