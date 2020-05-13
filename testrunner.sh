a7ddb1969832b28cf5bb1684f80660f20d739cd5
blokkendoos <jvo@chaosgeordend.nl>
2020-05-13 21:41:24 +0200

    Also clean PDF and TXT testoutput

--- testrunner.sh
+++ testrunner.sh
@@ -5,6 +5,8 @@ find . -name "*.pyc" -exec rm '{}' ';'
 
 # clean temporary files
 find ./tmp -name "*.aac" -exec rm '{}' ';'
+find ./tmp -name "*.txt" -exec rm '{}' ';'
+find ./tmp -name "*.pdf" -exec rm '{}' ';'
 
 # run the tests
 nosetests-3.7 -s tests/
