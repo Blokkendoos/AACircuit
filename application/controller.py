c0a0c09b45ba8e4d47d54009b536429640503e56
blokkendoos <jvo@chaosgeordend.nl>
2020-05-28 13:26:19 +0200

    Update translations

--- application/controller.py
+++ application/controller.py
@@ -10,7 +10,7 @@ import xerox
 import collections
 from pubsub import pub
 
-from application import _
+from locale import gettext as _
 from application import ERROR, WARNING
 from application import REMOVE, INSERT
 from application import ERASER, COMPONENT, CHARACTER, TEXT, COL, ROW, DRAW_RECT, LINE, MAG_LINE, DIR_LINE
